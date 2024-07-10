b7556a097397b58a1ef7b138d1881c860986ddb1
Eden Luski <luskieden@gmail.com>
2024-07-10 16:08:07 +0300

    font

---
+++ src/pages/CodeBlocks.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import Editor from "@monaco-editor/react";
+import { useParams } from "react-router-dom";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3001", {
+  transports: ["websocket", "polling", "flashsocket"],
+});
+const CodeBlocks = () => {
+  const { id } = useParams();
+  console.log(socket);
+  const [code, setCode] = useState("");
+  const [role, setRole] = useState("student");
+  const [solution, Setsolution] = useState("");
+  const [studentsCount, setStudentsCount] = useState(0);
+  const [isCorrect, setIscorrenct] = useState(false);
+  useEffect(() => {
+    socket.emit("join", { codeBlockId: id, isMentor: studentsCount === 0 });
+
+    socket.on("init", ({ initialCode, role, students }) => {
+      setCode(initialCode);
+      setRole(role);
+      setStudentsCount(students);
+    });
+
+    socket.on("codeUpdate", (newCode) => {
+      setCode(newCode);
+    });
+    socket.on("studentCount", (count) => {
+      setStudentsCount(count);
+    });
+    return () => {
+      socket.emit("leave", { codeBlockId: id });
+    };
+  },[id,studentsCount,solution,role]);
+  console.log(role)
+  const handleCodeChange = (newCode) => {
+    console.log(newCode);
+    setCode(newCode);
+  };
+
+  const handleSolutionChange = (newSolution) => {
+    Setsolution(newSolution);
+    socket.emit("solutionChange", { codeBlockId: id, newSolution });
+  };
+
+  console.log(id);
+  return (
+    <div>
+      {role === "student" && isCorrect && <p>V</p>}
+
+      <Editor
+        height="90vh"
+        defaultLanguage="javascript"
+        options={{readOnly:role==="mentor"}}
+        defaultValue="// some comment"
+        onChange={(value) => handleCodeChange(value)}
+      />
+      {role === "mentor" && (
+        <>
+          <h2>Solution</h2>
+          <Editor
+            height="90vh"
+            defaultLanguage="javascript"
+            defaultValue="// some comment"
+            onChange={(value) => handleSolutionChange(value)}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CodeBlocks;
---
+++ src/pages/Lobby.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import "./Lobby.css";
+import axios from "axios";
+const Lobby = () => {
+  const [codeBlocks, setCodeBlocks] = useState([]);
+
+  useEffect(() => {
+  
+    axios
+      .get("http://localhost:3001/api/codeblocks")
+      .then((response) => setCodeBlocks(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+  console.log(codeBlocks)
+  return (
+    <div className="my-style">
+      <h1>Welcome to the Lobby!</h1>
+      <div>
+        <h1>Choose code block</h1>
+        <ul>
+          {codeBlocks.map((block, index) => (
+            <li key={index}>
+              <Link to={`/codeblock/${block._id}`}>{block.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Lobby;
